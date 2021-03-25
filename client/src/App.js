e517033ff1fa2df2a03b614bcf4066a2294af9c0
Richard <richard.yang.tong@gmail.com>
2021-03-25 14:17:46 -0400

    Added testing form and button.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import axios from 'axios';
 
 function App() {
   return (
-    <div>
-      <button className="btn btn-danger">Hellow World!</button>
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <input 
+            type="text" 
+            className="input-control" 
+            placeholder="Search for Books..."
+            autoComplete="off"
+          />
+        </div>
+        <button type="submit" className="btn btn-danger">Search</button>
+      </form>
     </div>
   );
 }
