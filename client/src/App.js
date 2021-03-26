3343708fee0e8bff4da9d7279680a1f59cb167d9
Richard <richard.yang.tong@gmail.com>
2021-03-25 22:23:03 -0400

    Installed React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import NavBar from './components/NavBar';
 import Search from './components/Search';
 
 const App = () => {
   return (
-    <div className="container">
+    <div>
+      <NavBar />
       <Search />
     </div>
   );
