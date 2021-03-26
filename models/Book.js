9f2d5afd64f3e047f5cc8b4c58bb970607620e6a
Richard <richard.yang.tong@gmail.com>
2021-03-26 10:21:16 -0400

    Added a schema model for Books.

--- models/Book.js
+++ models/Book.js
@@ -2,3 +2,11 @@ const mongoose = require('mongoose');
 
 // Schema
 const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: String,
+    description: String,
+    authors: [String],
+    imageLink: String,
+    selfLink: String
+});
