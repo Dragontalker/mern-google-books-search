eb89edfb95e97dedec8c25bfdd8e8f10d4d6a63e
Richard <richard.yang.tong@gmail.com>
2021-03-25 21:33:59 -0400

    Use hooks to bind the search button with form input.

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,34 @@ import fetchData from './utils/api';
 import React, {useState, useEffect } from 'react';
 
 function App() {
+
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
   
-  useEffect(() => {
-    fetchData("javascript", 40).then(res => {
-      console.log(res);
-    })
-  }, [])
+  // useEffect(() => {
+  //   fetchData("javascript", 40).then(res => {
+  //     console.log(res);
+  //   })
+  // }, [])
+
+  const handleChange = (event) => {
+    const bookName = event.target.value;
+    setBook(bookName);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(book);
+  }
 
   return (
     <div className="container">
       <h1>Book Search Application</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <input 
             type="text" 
+            onChange={handleChange}
             className="form-control" 
             placeholder="Search for Books..."
             autoComplete="off"
