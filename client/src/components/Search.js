bdaea3e49b27103101ce8e41c0f2f23aa9bbc2e5
Richard <richard.yang.tong@gmail.com>
2021-03-25 22:13:52 -0400

    Moved search page to its own component.

---
+++ client/src/components/Search.js
@@ -0,0 +1,43 @@
+import fetchData from '../utils/api';
+import { useState } from 'react';
+
+const Search = () => {
+
+    const [book, setBook] = useState("");
+    const [result, setResult] = useState([]);
+    
+    const handleChange = (event) => {
+      const bookName = event.target.value;
+      setBook(bookName);
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      fetchData(book, 40).then(res => {
+        setResult(res.data.items);
+      })
+    }
+  
+    return (
+      <div className="container">
+        <h1>Book Search Application</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input 
+              type="text" 
+              onChange={handleChange}
+              className="form-control" 
+              placeholder="Search for Books..."
+              autoComplete="off"
+            />
+          </div>
+          <button type="submit" className="btn btn-danger">Search</button>
+        </form>
+        {JSON.stringify(result)}
+      </div>
+    );
+  }
+  
+  export default Search
+;
+  
