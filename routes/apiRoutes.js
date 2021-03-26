794653bc2207ac3bc14245dab326e14593b59de3
Richard <richard.yang.tong@gmail.com>
2021-03-26 11:09:48 -0400

    Added a get request handler in api router.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const Book = require('../models/Book');
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    Book.find({})
+        .then((data) => {
+            console.log("Data: ", data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log("Erro: ", error);
+        });
+});
 
 module.exports = router;
