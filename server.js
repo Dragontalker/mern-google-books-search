c908441c9f98fa62df74217b52d650a1b8f99961
Richard <richard.yang.tong@gmail.com>
2021-03-26 22:41:26 -0400

    Testing Heroku.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
-require('dotenv').config({ path: "./config.env" });
+// require('dotenv').config({ path: "./config.env" });
 
 const app = express();
 const PORT = process.env.PORT || 8080;
